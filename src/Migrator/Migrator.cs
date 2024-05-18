8ea94dbb50e5cd9dfba15653e5429f7709256e20
Ibragf <gaziibragim@mail.ru>
2024-05-18 20:46:22 +0300

    init: add migrator

---
+++ src/Migrator/Migrator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+using Microsoft.Extensions.Configuration;
+
+namespace Migrator;
+
+public class Handler
+{
+    public string FunctionHandler(byte[] a) 
+    {
+        var currentDirectory = Directory.GetCurrentDirectory();
+
+        var config = new ConfigurationBuilder()
+            .SetBasePath(currentDirectory)
+            .AddJsonFile("appsettings.json")
+            .AddEnvironmentVariables()
+            .Build();
+
+        return config["config"] ?? "empty";
+    }
+}
