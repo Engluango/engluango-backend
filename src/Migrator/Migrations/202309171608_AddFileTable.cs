c53b9ca168f4de33773479d1f94544b30ff9e908
Ibragf <gaziibragim@mail.ru>
2024-05-18 23:55:30 +0300

    Update 202309171608_AddFileTable.cs

--- src/Migrator/Migrations/202309171608_AddFileTable.cs
+++ src/Migrator/Migrations/202309171608_AddFileTable.cs
@@ -5,12 +5,8 @@ namespace Migrator.Migrations;
 [TimestampedMigration(2023, 9, 17, 16 ,8)]
 public sealed class AddFileTable : ForwardOnlyMigration 
 {
-    private const string CreateExtension = @"CREATE EXTENSION IF NOT EXISTS ""uuid-ossp"";";
-    
     public override void Up()
     {
-        Execute.Sql(CreateExtension);
-        
         Create.Table("File")
             .WithColumn("Id").AsGuid().WithDefault(SystemMethods.NewGuid).PrimaryKey()
             .WithColumn("VirtualName").AsString(75).NotNullable()
