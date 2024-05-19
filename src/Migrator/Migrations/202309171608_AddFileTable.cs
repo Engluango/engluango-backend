5589f6eecf8c62cb06881013f03f1084f3331ce0
Ibragf <gaziibragim@mail.ru>
2024-05-19 11:33:05 +0300

    Update 202309171608_AddFileTable.cs

--- src/Migrator/Migrations/202309171608_AddFileTable.cs
+++ src/Migrator/Migrations/202309171608_AddFileTable.cs
@@ -5,7 +5,9 @@ namespace Migrator.Migrations;
 [TimestampedMigration(2023, 9, 17, 16 ,8)]
 public sealed class AddFileTable : ForwardOnlyMigration 
 {
-    private const string CreateExtension = @"CREATE EXTENSION IF NOT EXISTS ""uuid-ossp"";";
+    private const string CreateExtension = @"
+        SET search_path to engluango;
+        CREATE EXTENSION IF NOT EXISTS ""uuid-ossp"";";
     
     public override void Up()
     {
