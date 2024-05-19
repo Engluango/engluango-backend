8d07875510cd65de014d84962d6d58cfa0e28048
Bugraev Gadzhi <65299728+Ibragf@users.noreply.github.com>
2024-05-19 20:20:30 +0300

    init: add migrator (#4)

---
+++ src/Migrator/Migrator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+using FluentMigrator.Runner;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace Migrator;
+
+public class Handler
+{
+    public string FunctionHandler(byte[] a) 
+    {
+        var serviceProvider = MigratorStartup.Setup();
+        var migrationRunner = serviceProvider.GetRequiredService<IMigrationRunner>();
+
+        migrationRunner.MigrateUp();
+
+        return "Successful migration";
+    }
+}
