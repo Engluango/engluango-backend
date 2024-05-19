6a26355b2895f8b120ee8b1d25a363936045984a
Ibragf <gaziibragim@mail.ru>
2024-05-19 11:29:02 +0300

    Update 202309171608_AddFileTable.cs

--- src/Migrator/Migrations/202309171608_AddFileTable.cs
+++ src/Migrator/Migrations/202309171608_AddFileTable.cs
@@ -10,12 +10,5 @@ public sealed class AddFileTable : ForwardOnlyMigration
     public override void Up()
     {
         Execute.Sql(CreateExtension);
-        
-        Create.Table("File")
-            .WithColumn("Id").AsGuid().WithDefault(SystemMethods.NewGuid).PrimaryKey()
-            .WithColumn("VirtualName").AsString(75).NotNullable()
-            .WithColumn("Path").AsString(300).NotNullable()
-            .WithColumn("AccessToken").AsString(50).Nullable().Indexed()
-            .WithColumn("ToDelete").AsBoolean().NotNullable().Indexed();
     }
 }
