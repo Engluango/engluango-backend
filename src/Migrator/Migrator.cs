1138525986a3a022807acbaeb21079c87317f190
Ibragf <gaziibragim@mail.ru>
2024-05-19 12:01:43 +0300

    Update Migrator.cs

--- src/Migrator/Migrator.cs
+++ src/Migrator/Migrator.cs
@@ -3,6 +3,7 @@ using System.IO;
 using FluentMigrator.Runner;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
 namespace Migrator;
 
@@ -12,6 +13,11 @@ public class Handler
     {
         var serviceProvider = MigratorStartup.Setup();
         var migrationRunner = serviceProvider.GetRequiredService<IMigrationRunner>();
+
+        var loggerFactory = serviceProvider.GetRequiredService<ILoggerFactory>();
+        var logger = loggerFactory.CreateLogger<Handler>();
+        
+        logger.LogDebug("DEBUG");
 
         Console.WriteLine("DEBUG");
         migrationRunner.MigrateUp();
